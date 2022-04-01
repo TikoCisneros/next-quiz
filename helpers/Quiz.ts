81dcd8f82833dedd6f553a2aeb2510e94f012785
Luis Cisneros <ext_lucisneros@falabella.cl>
2022-03-31 21:38:34 -0500

    Show data on score page

--- helpers/Quiz.ts
+++ helpers/Quiz.ts
@@ -1,6 +1,17 @@
-export const getNextQuestionID = (surveyIDs: number[], actualID: number): number => {
+export const getNextQuestionID = (
+  surveyIDs: number[],
+  actualID: number
+): number => {
   const nextIndex = surveyIDs.indexOf(actualID) + 1;
   return surveyIDs[nextIndex];
 };
 
-export const isLastQuestion = (surveyIDs: number[], actualID: number): boolean => surveyIDs[surveyIDs.length - 1] === actualID;
+export const isLastQuestion = (
+  surveyIDs: number[],
+  actualID: number
+): boolean => surveyIDs[surveyIDs.length - 1] === actualID;
+
+export const getQuizPercent = (
+  totalQuestions: number,
+  rightAnswers: number
+): string => `${Math.round((rightAnswers / totalQuestions) * 100)}%`;
--- pages/score.tsx
+++ pages/score.tsx
@@ -1,8 +1,23 @@
 import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { getQuizPercent } from '../helpers/Quiz';
 
 const Score: NextPage = () => {
+  const { query: { totalQuestions, rightAnswers } } = useRouter();
+
+  if (!totalQuestions || !rightAnswers) {
+    return null;
+  }
+
+  const percent = getQuizPercent(+totalQuestions, +rightAnswers);
+
   return (
-    <div>Score</div>
+    <div>
+      <h1>Survey Score</h1>
+      <div>{totalQuestions}</div>
+      <div>{rightAnswers}</div>
+      <div>{percent}</div>
+    </div>
   )
 }
 
